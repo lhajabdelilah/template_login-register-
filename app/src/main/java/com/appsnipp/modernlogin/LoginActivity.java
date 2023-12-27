a9a314f9ca94ddbdd81fdd6483322f7190703b4d
AITLHAJ2001 <a.aitlhaj2078@uca.ac.ma>
2023-12-27 17:42:36 +0100

    template mobile

---
+++ app/src/main/java/com/appsnipp/modernlogin/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.appsnipp.modernlogin;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // For changing status bar icon colors
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_login);
+
+        textView = findViewById(R.id.register);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/appsnipp/modernlogin/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.appsnipp.modernlogin;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+}
